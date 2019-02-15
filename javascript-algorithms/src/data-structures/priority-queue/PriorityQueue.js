c828feb4f39f8df3c19045db8ed3d530b44c6805
Emily <emily@xiaoman.cn>
2019-02-15 15:40:13 +0800

    feat: add priorityQueue

---
+++ javascript-algorithms/src/data-structures/priority-queue/PriorityQueue.js
@@ -0,0 +1,55 @@
+import MinHeap from '../heap/MinHeap';
+import Comparator from '../../utils/comparator/Comparator';
+
+export default class PriorityQueue extends MinHeap {
+  constructor() {
+    super();
+    this.priorities = {};
+    this.compare = new Comparator(this.comparePriority.bind(this));
+  }
+
+  add(item, priority = 0) {
+    this.priorities[item] = priority;
+    super.add(item);
+
+    return this;
+  }
+
+  remove(item, customerFindingComparator) {
+    super.remove(item, customerFindingComparator);
+    delete this.priorities[item];
+
+    return this;
+  }
+
+  changePriority(item, priority) {
+    this.remove(item, new Comparator(this.compareValue));
+    this.add(item, priority);
+
+    return this;
+  }
+
+  findByValue(item) {
+    return this.find(item, new Comparator(this.compareValue));
+  }
+
+  hasValue(item) {
+    return this.findByValue(item).length > 0;
+  }
+
+  comparePriority(a, b) {
+    if (this.priorities[a] === this.priorities[b]) {
+      return 0;
+    }
+
+    return this.priorities[a] < this.priorities[b] ? -1 : 1;
+  }
+
+  compareValue(a, b) {
+    if (a === b) {
+      return 0;
+    }
+
+    return a < b ? -1 : 1;
+  }
+}
