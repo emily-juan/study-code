0ce8a948a5b66b2b24b6fdae686cfa561299006e
Emily <emily@xiaoman.cn>
2019-02-19 15:08:07 +0800

    feat: add AvlTree

--- javascript-algorithms/src/data-structures/tree/avl-tree/AvlTree.js
+++ javascript-algorithms/src/data-structures/tree/avl-tree/AvlTree.js
@@ -39,4 +39,74 @@ export default class AvlTree extends BinarySearchTree {
       }
     }
   }
+
+  rotateLeftLeft(rootNode) {
+    const leftNode = rootNode.left;
+    rootNode.setLeft(null);
+
+    if (rootNode.parent) {
+      rootNode.parent.setLeft(leftNode);
+    } else if (rootNode === this.root) {
+      this.root = leftNode;
+    }
+
+    if (leftNode.right) {
+      rootNode.setLeft(leftNode.right);
+    }
+
+    leftNode.setRight(rootNode);
+  }
+
+  rotateLeftRight(rootNode) {
+    const leftNode = rootNode.left;
+    rootNode.setLeft(null);
+
+    const leftRightNode = leftNode.right;
+    leftNode.setRight(null);
+
+    if (leftRightNode.left) {
+      leftNode.setRight(leftRightNode.left);
+      leftRightNode.setLeft(null);
+    }
+
+    rootNode.setLeft(leftRightNode);
+    leftRightNode.setLeft(leftNode);
+
+    this.rotateLeftLeft(rootNode);
+  }
+
+  rotateRightLeft(rootNode) {
+    const rightNode = rootNode.right;
+    rootNode.setRight(null);
+
+    const rightLeftNode = rightNode.left;
+    rightNode.setLeft(null);
+
+    if (rightLeftNode.right) {
+      rightNode.setLeft(rightLeftNode.right);
+      rightLeftNode.setRight(null);
+    }
+
+    rootNode.setRight(rightLeftNode);
+    rightLeftNode.setRight(rightNode);
+
+    this.rotateRightRight(rootNode);
+  }
+
+  rotateRightRight(rootNode) {
+    const rightNode = rootNode.right;
+    rootNode.setRight(null);
+
+    if (rootNode.parent) {
+      rootNode.parent.setRight(rightNode);
+    } else if (this.root === rootNode) {
+      this.root = rightNode;
+    }
+
+    if (rightNode.left) {
+      rootNode.setRight(rightNode.left);
+    }
+
+    rightNode.setLeft(rootNode);
+  }
 }
