0703a6b4a124df4d90af427237e792f1947c0560
Emily <emily@xiaoman.cn>
2019-03-04 17:53:03 +0800

    feat: add some bits algorithms

---
+++ javascript-algorithms/src/algorithms/math/bits/updateBit.js
@@ -0,0 +1,5 @@
+export default function updateBit(number, bitPosition, bitValue) {
+  const bitValueNormalized = bitValue ? 1 : 0;
+  const clearMask = ~(1 << bitPosition);
+  return (number & clearMask) | (bitValueNormalized << bitPosition);
+}
