077f53eb1af50c5a6e435c27f3d0a55547dbf833
Emily <emily@xiaoman.cn>
2019-03-13 16:26:31 +0800

    feat: add primality-test

---
+++ javascript-algorithms/src/algorithms/math/primality-test/trialDivision.js
@@ -0,0 +1,29 @@
+export default function trialDivision(number) {
+  // Check if number is integer.
+  if (number % 1 !== 0) {
+    return false;
+  }
+
+  if (number <= 1) {
+    return false;
+  }
+
+  if (number <= 3) {
+    // All numbers from 2 to 3 are prime.
+    return true;
+  }
+
+  if (number % 2 === 0) {
+    return false;
+  }
+
+  // If there is no dividers up to square root of n then there is no higher dividers as well.
+  const dividerLimit = Math.sqrt(number);
+  for (let divider = 3; divider <= dividerLimit; divider += 2) {
+    if (number % divider === 0) {
+      return false;
+    }
+  }
+
+  return true;
+}
