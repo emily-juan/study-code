26117f28be67be2d382b28f4258ded7059a6be1b
Emily <emily@xiaoman.cn>
2019-08-16 11:37:09 +0800

    feat: add development

---
+++ webpack/development/webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: {
+    app: './src/index.js',
+    print: './src/print.js',
+  },
+  devtool: 'inline-source-map',
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [
+    new CleanWebpackPlugin,
+    new HtmlWebpackPlugin({
+      title: 'Output Management'
+    })
+  ],
+  output: {
+    // filename: 'bundle.js',
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    publicPath: '/',
+  },
+}
